0f0a01b30397cd955cc0e9a74dbc58804c542581
Alejandro Luzuriaga Rodríguez <alejandro.luzuriaga@siigroup-spain.com>
2026-06-04 13:56:13 +0200

    Configure custom domain for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
-const repo = "JuanAntonioTaxi";
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: isGitHubActions ? `/${repo}` : "",
-  assetPrefix: isGitHubActions ? `/${repo}/` : "",
   images: {
     unoptimized: true,
   },
